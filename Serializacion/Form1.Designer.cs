a969d6a7cc031b57fa47050dd0cbd0766efa205f
LeandrOS <trioxid88@gmail.com>
2018-05-03 21:00:26 -0500

    Add project files.

---
+++ Serializacion/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Serializacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSerializar = new System.Windows.Forms.Button();
+            this.btnDeserealizar = new System.Windows.Forms.Button();
+            this.btnComprimir = new System.Windows.Forms.Button();
+            this.btnDescomprimir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnGuardarComo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.dtFecNac = new System.Windows.Forms.DateTimePicker();
+            this.dgSalida = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSalida)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSerializar
+            // 
+            this.btnSerializar.Location = new System.Drawing.Point(12, 12);
+            this.btnSerializar.Name = "btnSerializar";
+            this.btnSerializar.Size = new System.Drawing.Size(82, 23);
+            this.btnSerializar.TabIndex = 0;
+            this.btnSerializar.Text = "Serializar";
+            this.btnSerializar.UseVisualStyleBackColor = true;
+            // 
+            // btnDeserealizar
+            // 
+            this.btnDeserealizar.Location = new System.Drawing.Point(12, 41);
+            this.btnDeserealizar.Name = "btnDeserealizar";
+            this.btnDeserealizar.Size = new System.Drawing.Size(82, 23);
+            this.btnDeserealizar.TabIndex = 1;
+            this.btnDeserealizar.Text = "Deserializar";
+            this.btnDeserealizar.UseVisualStyleBackColor = true;
+            // 
+            // btnComprimir
+            // 
+            this.btnComprimir.Location = new System.Drawing.Point(230, 12);
+            this.btnComprimir.Name = "btnComprimir";
+            this.btnComprimir.Size = new System.Drawing.Size(82, 23);
+            this.btnComprimir.TabIndex = 2;
+            this.btnComprimir.Text = "Comprimir";
+            this.btnComprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnDescomprimir
+            // 
+            this.btnDescomprimir.Location = new System.Drawing.Point(230, 41);
+            this.btnDescomprimir.Name = "btnDescomprimir";
+            this.btnDescomprimir.Size = new System.Drawing.Size(82, 23);
+            this.btnDescomprimir.TabIndex = 3;
+            this.btnDescomprimir.Text = "Descomprimir";
+            this.btnDescomprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(419, 12);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(97, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnGuardarComo
+            // 
+            this.btnGuardarComo.Location = new System.Drawing.Point(419, 41);
+            this.btnGuardarComo.Name = "btnGuardarComo";
+            this.btnGuardarComo.Size = new System.Drawing.Size(97, 23);
+            this.btnGuardarComo.TabIndex = 5;
+            this.btnGuardarComo.Text = "Guardar Como";
+            this.btnGuardarComo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Apellidos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(252, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Codigo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "FecNac:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(90, 79);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 10;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(90, 105);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidos.TabIndex = 11;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(333, 79);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(107, 20);
+            this.txtCodigo.TabIndex = 12;
+            // 
+            // dtFecNac
+            // 
+            this.dtFecNac.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFecNac.Location = new System.Drawing.Point(333, 105);
+            this.dtFecNac.Name = "dtFecNac";
+            this.dtFecNac.Size = new System.Drawing.Size(107, 20);
+            this.dtFecNac.TabIndex = 13;
+            // 
+            // dgSalida
+            // 
+            this.dgSalida.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgSalida.Location = new System.Drawing.Point(12, 151);
+            this.dgSalida.Name = "dgSalida";
+            this.dgSalida.Size = new System.Drawing.Size(504, 283);
+            this.dgSalida.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 446);
+            this.Controls.Add(this.dgSalida);
+            this.Controls.Add(this.dtFecNac);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuardarComo);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnDescomprimir);
+            this.Controls.Add(this.btnComprimir);
+            this.Controls.Add(this.btnDeserealizar);
+            this.Controls.Add(this.btnSerializar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgSalida)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSerializar;
+        private System.Windows.Forms.Button btnDeserealizar;
+        private System.Windows.Forms.Button btnComprimir;
+        private System.Windows.Forms.Button btnDescomprimir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnGuardarComo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.DateTimePicker dtFecNac;
+        private System.Windows.Forms.DataGridView dgSalida;
+    }
+}
+
